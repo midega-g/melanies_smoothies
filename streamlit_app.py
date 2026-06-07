ad6e69dfe6233a5f5dc97fb0da29656d46ac7d63
George Midega <67196846+midega-g@users.noreply.github.com>
2026-06-07 10:33:31 +0300

    Update dataframe selection and display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ session = cnx.session()
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('fruit_name'))
-# st.dataframe(data = my_dataframe, use_container_width=True)
+my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('search_on'))
+st.dataframe(data = my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
