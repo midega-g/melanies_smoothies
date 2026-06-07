c58d2600a1bf4a196d8ea854d40607f18e0a0268
George Midega <67196846+midega-g@users.noreply.github.com>
2026-06-07 10:36:30 +0300

    Add Pandas conversion for DataFrame display
    Converted Snowpark DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -18,7 +19,12 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('search_on'))
-st.dataframe(data = my_dataframe, use_container_width=True)
+# st.dataframe(data = my_dataframe, use_container_width=True)
+# st.stop()
+
+# convert the snowpark dataframe to a pandas dataframe so we can use the loc function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
